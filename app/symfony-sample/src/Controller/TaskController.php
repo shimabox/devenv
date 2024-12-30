c702ce2ec886435e9366edd5163567163a2527ea
shimabox <shimabox@gmail.com>
2024-12-30 18:40:23 +0900

    feat: Createの作成まで

--- app/symfony-sample/src/Controller/TaskController.php
+++ app/symfony-sample/src/Controller/TaskController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Task;
+use App\Form\TaskType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,5 +17,22 @@ class TaskController extends AbstractController
     {
         $tasks = $em->getRepository(Task::class)->findAll();
         return $this->render('task/index.html.twig', ['tasks' => $tasks]);
+    }
+
+    #[Route('/task/new', name: 'task_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $task = new Task();
+        $form = $this->createForm(TaskType::class, $task);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($task);
+            $em->flush();
+
+            return $this->redirectToRoute('task_index');
+        }
+
+        return $this->render('task/new.html.twig', ['form' => $form->createView()]);
     }
 }
