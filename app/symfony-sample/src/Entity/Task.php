232c464ce43b8ecf908909c6b36c5eeda6b324e3
shimabox <shimabox@gmail.com>
2024-12-31 10:16:41 +0900

    feat: make:entity Task で作成された状態に戻す(c285144 を戻す)
    Qiitaから
    - EntityのプロパティをPHPレベルでnon-nullableにするのは（make:entityのデフォルトがnullableになっていることからも）最近のSymfonyコミュニティの慣習からは外れている

--- app/symfony-sample/src/Entity/Task.php
+++ app/symfony-sample/src/Entity/Task.php
@@ -12,20 +12,20 @@ class Task
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    private int $id;
+    private ?int $id = null;
 
     #[ORM\Column(length: 255)]
-    private string $title;
+    private ?string $title = null;
 
     #[ORM\Column(type: Types::TEXT, nullable: true)]
     private ?string $description = null;
 
-    public function getId(): int
+    public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getTitle(): string
+    public function getTitle(): ?string
     {
         return $this->title;
     }
